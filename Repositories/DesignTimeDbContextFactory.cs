67827047393752b3753374ba9d8e4ff233bfb667
Diego Cabrera <juandcabreraalvarez@gmail.com>
2025-10-29 19:09:14 -0400

    add design ef core

---
+++ Repositories/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Repositories
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        
+            builder.UseSqlServer(
+                "Server=localhost\\SQLEXPRESS;Database=Nexocorp;MultipleActiveResultSets=True;Trusted_Connection=True;TrustServerCertificate=True");
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
